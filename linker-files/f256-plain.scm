66bff5c5bc4b0b64bd312c716b8bb57440ced41f
Håkan Thörngren <hth313@gmail.com>
2023-06-09 16:11:34 -0700

    Update linker plain linker file
    Tell the linker that the zero page memory is matched with the zpage
    memory space qualifier.
    
    Also exclude the stack area from the memories and rely on having the
    stack set up by TinyCore. We do not define a stack at all. If this
    area is provided the linker will incorrectly think it can place things
    in this 0100-01ff area which is highly unsafe.

--- linker-files/f256-plain.scm
+++ linker-files/f256-plain.scm
@@ -1,4 +1,4 @@
 (define memories
-  '((memory zeroPage (address (#x10 . #xef)) (type ram))
-    (memory RAM (address (#x0100 . #xbfff)) (type any))
+  '((memory zeroPage (address (#x10 . #xef)) (type ram) (qualifier zpage))
+    (memory RAM (address (#x0200 . #xbfff)) (type any))
     ))
