e5b68491ec3618b8f55d544b2a599989b1f45f97
Håkan Thörngren <hth313@gmail.com>
2023-06-26 22:49:16 -0700

    Update start address in the default linker file

--- linker-files/f256-plain.scm
+++ linker-files/f256-plain.scm
@@ -1,4 +1,4 @@
 (define memories
   '((memory zeroPage (address (#x10 . #xef)) (type ram) (qualifier zpage))
-    (memory RAM (address (#x0200 . #xbfff)) (type any))
+    (memory RAM (address (#x02000 . #xbfff)) (type any))
     ))
